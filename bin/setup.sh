98ca9ca499c2b1f57616ece33e66b3ed943e2efa
Radu Helstern <radu.helstern@gmail.com>
2014-05-12 02:17:00 +0100

    fixed wrong variable assignment

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,13 +2,12 @@
 
 #inspired by https://github.com/mathiasbynens/dotfiles.git
 
-
 #find the root dir (relative to the one this script is located in) and make it the working dir
 ROOT_DIR='';
-if [ -z type realpath > /dev/null 2>$1 ]; then
-    ROOT_DIR=dirname "$(dirname $(realpath "${BASH_SOURCE}"))"
+if [ -z type realpath >/dev/null 2>&1 ]; then
+    ROOT_DIR=$(dirname "$(dirname $(realpath "${BASH_SOURCE}"))")
 else
-    ROOT_DIR=dirname "$(dirname $(readlink -f "${BASH_SOURCE}"))"
+    ROOT_DIR=$(dirname "$(dirname $(readlink -f "${BASH_SOURCE}"))")
 fi
 cd "$ROOT_DIR"
 
@@ -17,17 +16,17 @@ git pull origin master
 
 function install() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bin/" --exclude "README.md" \
-		-av --no-perms \
-		. ~
-	source ~/.bash_profile
+        -av --no-perms \
+        . ~
+    source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	install
+    install
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		install
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        install
+    fi
 fi
 unset install
